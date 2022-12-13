adb49395b47045957d4bfe504623aced2db4f3fd
ferusel <wheatgrass.mc@gmail.com>
2022-12-13 16:05:28 +0800

    Add CustomLogo component

---
+++ pages/components/utilities/CustomLogo.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { createStyles } from "@mantine/core";
+import { ImageSize } from "../../types/utilities/ImageSize";
+
+interface CustomLogoProps {
+  imgSrc: string;
+  size: ImageSize;
+}
+
+const useStyles = createStyles((theme, { height, width }: ImageSize) => ({
+  logo: {
+    objectFit: "contain",
+  },
+  logoContainer: {
+    position: "relative",
+    width,
+    height,
+  },
+}));
+
+function CustomLogo(props: CustomLogoProps) {
+  const {
+    imgSrc,
+    size: { width, height },
+  } = props;
+  const { classes } = useStyles({ width, height });
+  return (
+    <div className={classes.logoContainer}>
+      <Image className={classes.logo} src={imgSrc} alt={"some logo"} fill />
+    </div>
+  );
+}
+
+export default CustomLogo;
