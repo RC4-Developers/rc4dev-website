b406b38b88a6c2bcb5ea8b46ffd272e6dcf20fd1
ferusel <wheatgrass.mc@gmail.com>
2022-12-13 15:20:54 +0800

    Add config file for HeaderMenu

---
+++ pages/config/HeaderMenu.tsx
@@ -0,0 +1,22 @@
+import { HeaderProps } from "../components/utilities/HeaderMenu";
+
+export const CONFIG_HEADERMENU: HeaderProps = {
+  links: [
+    {
+      link: "/home",
+      label: "Home",
+    },
+    {
+      link: "/aboutUs",
+      label: "About Us",
+    },
+    {
+      link: "/projects",
+      label: "Projects",
+    },
+    {
+      link: "/contact",
+      label: "Contact",
+    },
+  ],
+};
