2677ae1f29c8fbdd1209e1179d786937336b484c
ferusel <wheatgrass.mc@gmail.com>
2022-12-15 12:09:10 +0800

    Change header and footer height

--- pages/config/viewport/HeaderMenu.tsx
+++ pages/config/viewport/HeaderMenu.tsx
@@ -1,7 +1,7 @@
 import { HeaderMenuProps } from "../../components/viewport/HeaderMenu";
 import { ImageSize } from "../../types/utilities/ImageSize";
 
-export const CONFIG_HEADER_FOOTER_HEIGHT = 100;
+export const CONFIG_HEADER_FOOTER_HEIGHT = 122;
 
 export const CONFIG_HEADERMENU: HeaderMenuProps = {
   links: [
