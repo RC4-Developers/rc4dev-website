fa7a21001dca566680d4e204b170e60afb54e8e3
ferusel <wheatgrass.mc@gmail.com>
2022-12-15 11:08:26 +0800

    Create config for website theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import { CONFIG_WEBSITE_THEME } from "./config/global/App";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider
-      theme={{ fontFamily: "Readex Pro, sans-serif" }}
-      withGlobalStyles
-      withNormalizeCSS
-    >
+    <MantineProvider theme={CONFIG_WEBSITE_THEME} withGlobalStyles withNormalizeCSS>
       <Component {...pageProps} />
     </MantineProvider>
   );
