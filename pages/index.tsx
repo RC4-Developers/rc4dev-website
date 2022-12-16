5e0d796cfe4209d16a48cf73ba62e0298d99db4c
ferusel <wheatgrass.mc@gmail.com>
2022-12-16 19:56:48 +0800

    Add hero to home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,47 @@
-import HeaderMenu from "./components/viewport/HeaderMenu";
-import { CONFIG_HEADERMENU } from "./config/HeaderMenu";
-import { Container, createStyles } from "@mantine/core";
+import { Container, createStyles, Group, Text, Title } from "@mantine/core";
+import { ColorStrip } from "./components/utilities/ColorStrip";
+import CustomLogo from "./components/utilities/CustomLogo";
 
-const useStyles = createStyles(() => ({
+const useStyles = createStyles((theme) => ({
   root: {
-    padding: 0,
+    paddingLeft: "0%",
+    paddingRight: "0%",
+    position: "relative",
+    overflow: "hidden",
+  },
+  hero: {
+    backgroundColor: theme.colors.rc4teal[9],
+    color: "white",
+    paddingTop: "3%",
+    paddingBottom: "2%",
+    paddingLeft: "3%",
+  },
+  heroTitle: {
+    width: "80%",
+  },
+  heroText: {},
+  gearIcon: {
+    transform: "translateY(-10%)",
+    backgroundColor: "#FFFFFF",
   },
 }));
 
-export default function Viewport(props) {
+export default function Homepage() {
   const { classes } = useStyles();
   return (
     <Container fluid className={classes.root}>
-      <HeaderMenu links={CONFIG_HEADERMENU.links} />
-      {props.children}
+      <Group className={classes.hero}>
+        <Title size={98} className={classes.heroTitle}>
+          We are RC4Developers.
+        </Title>
+        <Text size={36} className={classes.heroText}>
+          Systems Thinking, Making the Future
+        </Text>
+        <ColorStrip height={"25%"} color={"rc4teal"} translateX={"25%"} dropShadow />
+        <ColorStrip height={"25%"} color={"rc4yellow"} translateX={"34.3%"} />
+        <ColorStrip height={"25%"} color={"rc4red"} translateX={"43.5%"} />
+        <CustomLogo imgSrc={"/gear.png"} size={{ width: 80, height: 80 }} />
+      </Group>
     </Container>
   );
 }
