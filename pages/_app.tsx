35081cbf550f0c55f50aa1fa32ab6925f1f6b069
ferusel <wheatgrass.mc@gmail.com>
2022-12-17 14:06:20 +0800

    Refactor header and footer to be in index.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,12 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
-import { Container, createStyles, MantineProvider } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import { CONFIG_WEBSITE_THEME } from "config/global/App";
-import HeaderMenu from "pages/components/viewport/HeaderMenu";
-import { CONFIG_HEADERMENU } from "config/viewport/HeaderMenu";
-import FooterMenu from "pages/components/viewport/FooterMenu";
-import { CONFIG_FOOTERMENU } from "config/viewport/FooterMenu";
-
-const useStyles = createStyles(() => ({
-  root: {
-    padding: 0,
-    height: "fit-content",
-  },
-}));
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { classes } = useStyles();
   return (
     <MantineProvider theme={CONFIG_WEBSITE_THEME} withGlobalStyles withNormalizeCSS>
-      <Container fluid className={classes.root}>
-        <HeaderMenu links={CONFIG_HEADERMENU.links} />
-        <Component {...pageProps} />
-        <FooterMenu links={CONFIG_FOOTERMENU.links} />
-      </Container>
+      <Component {...pageProps} />
     </MantineProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,17 @@ import { Container, createStyles } from "@mantine/core";
 import HomepageHero from "pages/components/homepage/HomepageHero";
 import HomepageNavigationRow from "pages/components/homepage/HomepageNavigationRow";
 import HomepageNewsBulletinHeader from "pages/components/homepage/HomepageNewsBulletinHeader";
+import HeaderMenu from "pages/components/viewport/HeaderMenu";
+import { CONFIG_HEADERMENU } from "config/viewport/HeaderMenu";
+import FooterMenu from "pages/components/viewport/FooterMenu";
+import { CONFIG_FOOTERMENU } from "config/viewport/FooterMenu";
 
 const useStyles = createStyles(() => ({
+  container: {
+    padding: 0,
+    height: "fit-content",
+  },
+
   root: {
     paddingLeft: "0%",
     paddingRight: "0%",
@@ -18,10 +27,14 @@ const useStyles = createStyles(() => ({
 export default function Homepage() {
   const { classes } = useStyles();
   return (
-    <Container fluid className={classes.root}>
-      <HomepageHero />
-      <HomepageNavigationRow />
-      <HomepageNewsBulletinHeader />
+    <Container fluid className={classes.container}>
+      <HeaderMenu links={CONFIG_HEADERMENU.links} />
+      <Container fluid className={classes.root}>
+        <HomepageHero />
+        <HomepageNavigationRow />
+        <HomepageNewsBulletinHeader />
+      </Container>
+      <FooterMenu links={CONFIG_FOOTERMENU.links} />
     </Container>
   );
 }
