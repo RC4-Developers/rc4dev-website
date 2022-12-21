c35010509f5c6d34196ebf2860d83ffe0bb860e7
ferusel <wheatgrass.mc@gmail.com>
2022-12-21 14:09:33 +0800

    Adjust styles for MemberRow

--- components/about-us/Members/MemberRow.tsx
+++ components/about-us/Members/MemberRow.tsx
@@ -1,4 +1,4 @@
-import { Avatar, createStyles, Flex, Grid, Text } from "@mantine/core";
+import { Card, createStyles, Flex, Grid, Image, Text } from "@mantine/core";
 
 export interface AvatarInfo {
   personName: string;
@@ -15,25 +15,49 @@ const useStyles = createStyles(() => ({
     flexDirection: "column",
     alignItems: "center",
   },
+  title: {
+    fontSize: "1.5rem",
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: "1rem",
+    textAlign: "center",
+  },
+  card: {
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "stretch",
+    alignItems: "stretch",
+    borderRadius: "10px",
+  },
+  textContainer: {
+    height: "100%",
+    flex: 1,
+  },
 }));
 
 export default function MemberRow(props: MemberRowProps) {
   const { classes } = useStyles();
   return (
-    <Grid>
+    <Grid gutter={40} align={"stretch"}>
       {props.people.map((person) => (
         <Grid.Col span={3} key={person.profileUrl}>
-          <Flex className={classes.person}>
-            <Avatar
-              src={person.profileUrl}
-              alt={person.personName}
-              size={200}
-              radius={200}
-              key={person.profileUrl}
-            />
-            <Text>{person.personName}</Text>
-            <Text>{person.personDesc}</Text>
-          </Flex>
+          <Card shadow={"sm"} radius={5} className={classes.card} withBorder>
+            <Card.Section>
+              <Image src={person.profileUrl} alt={person.personName} />
+            </Card.Section>
+            <Flex
+              className={classes.textContainer}
+              direction={"column"}
+              align={"center"}
+              justify={"space-between"}
+            >
+              <Text className={classes.title}>{person.personName}</Text>
+              <Text className={classes.subtitle}>{person.personDesc}</Text>
+            </Flex>
+          </Card>
         </Grid.Col>
       ))}
     </Grid>
