72602c446e6c2d847558d2604a422e037a1b3765
ferusel <wheatgrass.mc@gmail.com>
2022-12-21 15:02:13 +0800

    Modify project dummy data

--- config/projects/ProjectsContainer.tsx
+++ config/projects/ProjectsContainer.tsx
@@ -1,27 +1,64 @@
-export const CONFIG_PROJECTS = [
+import { YearlyProjectInfo } from "components/projects/Projects/ProjectsContainer";
+
+export const CONFIG_PROJECTS: YearlyProjectInfo[] = [
   {
-    projectName: "Project 1",
-    projectDesc: "Project 1 Description",
-    projectUrl: "https://picsum.photos/200/300",
+    year: 2023,
+    projects: [
+      {
+        projectName: "Project 1",
+        projectDesc: "Project 1 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+      {
+        projectName: "Project 2",
+        projectDesc: "Project 2 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+      {
+        projectName: "Project 3",
+        projectDesc: "Project 3 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+      {
+        projectName: "Project 4",
+        projectDesc: "Project 4 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+      {
+        projectName: "Project 5",
+        projectDesc: "Project 5 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+    ],
   },
   {
-    projectName: "Project 2",
-    projectDesc: "Project 2 Description",
-    projectUrl: "https://picsum.photos/200/300",
-  },
-  {
-    projectName: "Project 3",
-    projectDesc: "Project 3 Description",
-    projectUrl: "https://picsum.photos/200/300",
-  },
-  {
-    projectName: "Project 4",
-    projectDesc: "Project 4 Description",
-    projectUrl: "https://picsum.photos/200/300",
-  },
-  {
-    projectName: "Project 5",
-    projectDesc: "Project 5 Description",
-    projectUrl: "https://picsum.photos/200/300",
+    year: 2022,
+    projects: [
+      {
+        projectName: "Project 1",
+        projectDesc: "Project 1 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+      {
+        projectName: "Project 2",
+        projectDesc: "Project 2 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+      {
+        projectName: "Project 3",
+        projectDesc: "Project 3 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+      {
+        projectName: "Project 4",
+        projectDesc: "Project 4 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+      {
+        projectName: "Project 5",
+        projectDesc: "Project 5 Description",
+        projectUrl: "https://picsum.photos/200/300",
+      },
+    ],
   },
 ];
