082433ea2747230fc82217b35af56c40ab5ddee3
ferusel <wheatgrass.mc@gmail.com>
2022-12-21 14:41:15 +0800

    Add dummy data for projects

---
+++ config/projects/ProjectsContainer.tsx
@@ -0,0 +1,27 @@
+export const CONFIG_PROJECTS = [
+  {
+    projectName: "Project 1",
+    projectDesc: "Project 1 Description",
+    projectUrl: "https://picsum.photos/200/300",
+  },
+  {
+    projectName: "Project 2",
+    projectDesc: "Project 2 Description",
+    projectUrl: "https://picsum.photos/200/300",
+  },
+  {
+    projectName: "Project 3",
+    projectDesc: "Project 3 Description",
+    projectUrl: "https://picsum.photos/200/300",
+  },
+  {
+    projectName: "Project 4",
+    projectDesc: "Project 4 Description",
+    projectUrl: "https://picsum.photos/200/300",
+  },
+  {
+    projectName: "Project 5",
+    projectDesc: "Project 5 Description",
+    projectUrl: "https://picsum.photos/200/300",
+  },
+];
