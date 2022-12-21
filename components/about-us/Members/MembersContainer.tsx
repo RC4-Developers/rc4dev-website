ffc27de4c95502fd988563d7e449e552729c2375
ferusel <wheatgrass.mc@gmail.com>
2022-12-21 14:40:29 +0800

    Removed unused CSS style

--- components/about-us/Members/MembersContainer.tsx
+++ components/about-us/Members/MembersContainer.tsx
@@ -4,15 +4,9 @@ import { CONFIG_EXCO, CONFIG_MEMBERS } from "config/about-us/MembersContainer";
 
 const useStyles = createStyles(() => ({
   root: {
-    flexGrow: 1,
     padding: "1rem",
     marginTop: "1rem",
     marginBottom: "1rem",
-  },
-  subtitle: {
-    fontSize: "1.5rem",
-    fontWeight: "bold",
-    marginBottom: "0.7em",
   },
   title: {
     fontSize: "2rem",
